4b1057829d455f9dac2c456bf4e787114ce8a4a7
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2023-08-26 22:35:04 +0300

    add ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import { Global } from './components/globalStyles';
+import ThemeAppProvider from './components/App/providers/ThemeProvider/ThemeProvider';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+    <Provider store={store}>
+        {/* <ThemeAppProvider> */}
+            <ErrorBoundary>
+                {/* <Global /> */}
+                <App />
+            </ErrorBoundary>
+        {/* </ThemeAppProvider> */}
+    </Provider>
+);
